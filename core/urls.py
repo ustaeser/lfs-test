diefenbach/django-lfs

sitemaps: add core functionality for pluggable/adaptable sitemaps
Add handling of optional LFS_SITEMAP setting in sitemap and urls. Integrator is now able to add completely own classes or override default settings for changefreq, priority and protocol.

lfs/core/urls.py
@@ -1,15 +1,12 @@
-# django imports
+
+from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.contrib.sitemaps import views as sitemap_views
 
 from . import views
-from lfs.core.sitemap import CategorySitemap
-from lfs.core.sitemap import PageSitemap
-from lfs.core.sitemap import ProductSitemap
-from lfs.core.sitemap import ShopSitemap
-
+from . utils import import_symbol
 
 urlpatterns = [
     # Auth
@@ -35,7 +32,38 @@ urlpatterns = [
 
     # Robots
     url(r'^robots.txt$', views.TextTemplateView.as_view(template_name='lfs/shop/robots.txt')),
+]
 
-    # Sitemaps
-    url(r'^sitemap.xml$', sitemap_views.sitemap, {'sitemaps': {"products": ProductSitemap, "categories": CategorySitemap, "pages": PageSitemap, "shop": ShopSitemap}}),
+# Sitemap urls
+try:
+    product_sitemap = import_symbol(settings.LFS_SITEMAPS["product"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import ProductSitemap
+    product_sitemap = ProductSitemap
+
+try:
+    category_sitemap = import_symbol(settings.LFS_CATEGORY_SITEMAP["categoy"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import CategorySitemap
+    category_sitemap = CategorySitemap
+
+try:
+    page_sitemap = import_symbol(settings.LFS_PAGE_SITEMAP["page"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import PageSitemap
+    page_sitemap = PageSitemap
+
+try:
+    shop_sitemap = import_symbol(settings.LFS_SHOP_SITEMAP["shop"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import ShopSitemap
+    shop_sitemap = ShopSitemap
+
+urlpatterns += [
+    url(r'^sitemap.xml$', sitemap_views.sitemap, {'sitemaps': {
+        "products": product_sitemap,
+        "categories": category_sitemap,
+        "pages": page_sitemap,
+        "shop": shop_sitemap,
+    }}),
 ]

ustaeser/lfs-test

core/urls.py
@@ -1,15 +1,12 @@
-# django imports
+
+from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.contrib.sitemaps import views as sitemap_views
 
 from . import views
-from lfs.core.sitemap import CategorySitemap
-from lfs.core.sitemap import PageSitemap
-from lfs.core.sitemap import ProductSitemap
-from lfs.core.sitemap import ShopSitemap
-
+from . utils import import_symbol
 
 urlpatterns = [
     # Auth
@@ -35,7 +32,38 @@ urlpatterns = [
 
     # Robots
     url(r'^robots.txt$', views.TextTemplateView.as_view(template_name='lfs/shop/robots.txt')),
+]
 
-    # Sitemaps
-    url(r'^sitemap.xml$', sitemap_views.sitemap, {'sitemaps': {"products": ProductSitemap, "categories": CategorySitemap, "pages": PageSitemap, "shop": ShopSitemap}}),
+# Sitemap urls
+try:
+    product_sitemap = import_symbol(settings.LFS_SITEMAPS["product"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import ProductSitemap
+    product_sitemap = ProductSitemap
+
+try:
+    category_sitemap = import_symbol(settings.LFS_SITEMAPS["category"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import CategorySitemap
+    category_sitemap = CategorySitemap
+
+try:
+    page_sitemap = import_symbol(settings.LFS_SITEMAPS["page"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import PageSitemap
+    page_sitemap = PageSitemap
+
+try:
+    shop_sitemap = import_symbol(settings.LFS_SITEMAPS["shop"]["sitemap"])
+except (AttributeError, KeyError, ImportError):
+    from . sitemap import ShopSitemap
+    shop_sitemap = ShopSitemap
+
+urlpatterns += [
+    url(r'^sitemap.xml$', sitemap_views.sitemap, {'sitemaps': {
+        "products": product_sitemap,
+        "categories": category_sitemap,
+        "pages": page_sitemap,
+        "shop": shop_sitemap,
+    }}),
 ]